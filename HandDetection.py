507564e6a8c9801e9a380a02962d668feed84bd6
Oğuz Muslu <oguzzmuslu@gmail.com>
2021-12-02 19:19:46 +0300

    Root name change

---
+++ HandDetection.py
@@ -0,0 +1,30 @@
+import mediapipe as mp
+import cv2
+import numpy as np
+
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+
+cap = cv2.VideoCapture(0)
+with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) as hands:
+    while cap.isOpened():
+        ret, frame = cap.read()
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image = cv2.flip(image, 1)
+        image.flags.writeable = False
+        results = hands.process(image)
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        #print(results)
+        if results.multi_hand_landmarks:
+            for num, hand in enumerate(results.multi_hand_landmarks):
+                mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS,
+                                          mp_drawing.DrawingSpec(color=(217, 133, 0), thickness=2, circle_radius=4),
+                                          mp_drawing.DrawingSpec(color=(105, 0, 101), thickness=2, circle_radius=2),)
+                cv2.imshow('HandTracking', image)
+                if cv2.waitKey(10) & 0xFF == ord('q'):
+                    break
+cap.release()
+cv2.destroyAllWindows()
+mp_drawing.DrawingSpec()
+
